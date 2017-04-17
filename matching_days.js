9b248c7eca67c98ed42054150625ad52a5660323
Thembeka <thembiegeorge@gmail.com>
2017-04-17 23:51:35 +0200

    added functionality such as color change when date is chosen, created css classes for the dates, added even listeners for the two inputs,removed background color for the day bar since it was not working properly and etc

--- matching_days.js
+++ matching_days.js
@@ -1,27 +1,35 @@
-var day1;
-var day2;
-var date1 = document.getElementById('date1').value;
-var date2 = document.getElementById('date2').value;
 
-// date1.addEventListener('click',checkDay);
-// date2.addEventListener('click',checkDay);
+var weekday = document.querySelectorAll('.day');
+
+var date1 = document.getElementById('date1');
+var date2 = document.getElementById('date2');
+
+ date1.addEventListener('change',checkDay);
+ date2.addEventListener('change',checkDay);
 
 function checkDay(){
+'use strict'
 
- // var date1 = document.getElementById('date1').value;
- // var date2 = document.getElementById('date2').value;
+    for (var i = 0; i < weekday.length; i++) {
+      if(date1.value !==""){
+  		var formattedDate1 = new Date(date1.value);
+  		var dayOfDate1 = formattedDate1.getDay();
+  	    }
+     if (date2.value !==""){
+    		var formattedDate2 = new Date(date2.value);
+    		 var dayOfDate2 = formattedDate2.getDay();
+    	    }
+		if(dayOfDate1 === dayOfDate2){
+			weekday[dayOfDate1].classList.add('green');
+		}
 
- var formattedDate1 = new Date (date1);
- var formattedDate2 = new Date (date2);
-
- var dayOfDate1 = formattedDate1.getDay();
-
- var dayOfDate2 = formattedDate2.getDay();
-
- if (dayOfDate1 == 0 || dayOfDate2 == 0){
-   window.alert("it kinda works");
- }
- date1.addEventListener('click',checkDay);
-date2.addEventListener('click',checkDay);
+		if(dayOfDate1 !== dayOfDate2 && dayOfDate1 !== undefined){
+			weekday[dayOfDate1].classList.add('yellow');
 
 }
+		if(dayOfDate1 !== dayOfDate2 && dayOfDate2 !== undefined){
+			weekday[dayOfDate2].classList.add('brown');
+		}
+
+        }
+    }
